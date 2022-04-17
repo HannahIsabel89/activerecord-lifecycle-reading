7fe89d8a018213f45f0802292fe252aa5cfdd89b
Hannah <hannahwoodward@hotmail.com>
2022-04-17 10:13:08 +0800

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,12 @@ class Post < ActiveRecord::Base
   belongs_to :author
   validate :is_title_case 
 
+  # before_save :make_title_case
+  # before_save is called after validation occurs
+  before_validation :make_title_case
+  #code works, which title cases the title, then the validation runs, which passes!
+
+
   private
 
   def is_title_case
